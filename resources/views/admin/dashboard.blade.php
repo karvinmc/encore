fdf28f628e5027a0c917a69db24d9b731230f1b8
karvinmc <135813602+karvinmc@users.noreply.github.com>
2025-06-24 09:30:43 +0700

    dashboard fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,12 +2,10 @@
 @section('title', 'Admin Dashboard')
 
 @section('content')
-  <div class="flex min-h-screen bg-gray-100">
-    <div class="flex-1 flex flex-col">
-      <main class="p-6 bg-white shadow-md rounded-md m-4">
-        <h1 class="text-2xl font-semibold mb-4">Welcome to the Admin Dashboard</h1>
-        <p class="text-gray-700">Manage users, concerts, tickets, and more from here.</p>
-      </main>
-    </div>
+  <div class="flex flex-col">
+    <main class="p-6 bg-white shadow-md rounded-md m-4">
+      <h1 class="text-2xl font-semibold mb-4">Welcome to the Admin Dashboard</h1>
+      <p class="text-gray-700">Manage users, concerts, tickets, and more from here.</p>
+    </main>
   </div>
 @endsection
